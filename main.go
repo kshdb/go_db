d6b6eb4a1e53a4d1ed7e1b92403cd909aaeb1165
sxh <123456>
2022-06-28 11:04:33 +0800

    测试mysql

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 			Pwd:    "haosql",
 			IP:     "127.0.0.1",
 			Port:   3306,
-			DbName: "blog",
+			DbName: "test",
 		},
 		QuerySql: "select id,title,path from post limit 10",
 	}
